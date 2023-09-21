ac80db7d2c5610e722ac4d0ef4c2ff23e8503a89
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-09-21 12:23:14 +0300

    Bump deps Add jest instead of jasmine

--- spec/helpers/jsdom.ts
+++
@@ -1,7 +0,0 @@
-import {JSDOM} from 'jsdom';
-
-const dom = new JSDOM('<html><body></body></html>')
-declare const global: any
-global.document = dom.window.document
-global.window = dom.window
-global.navigator = dom.window.navigator
