2bc6bd72355a914aab3f322d0ea6819824e454bb
Yosbel <yosbelms@users.noreply.github.com>
2024-11-30 14:29:15 -0300

    update deps

--- spec/helpers/enzyme.ts
+++
@@ -1,7 +0,0 @@
-import * as jasmineEnzyme from 'jasmine-enzyme'
-import { configure } from 'enzyme'
-import * as Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
-
-beforeEach(() => jasmineEnzyme())
